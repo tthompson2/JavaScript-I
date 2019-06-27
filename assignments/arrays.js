059dff64d78e4f7c5c8d60a7a57a134dbdc43f86
Trevor Thompson <trevthom18@gmail.com>
2019-06-26 22:45:20 -0400

    Question 6 a

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -114,5 +114,12 @@ console.log(oldCars);
 let BMWAndAudi = [];
 console.log();
 
+for(let i = 0; i < inventory.length; i++)
+{
+    if(inventory[i].car_make === "BMW" || inventory[i].car_make === "Audi")
+    {
+        BMWAndAudi+=inventory[i] + " ";
+    }
+}
 
-
+console.log(JSON.stringify(BMWAndAudi));
